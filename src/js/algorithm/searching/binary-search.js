41fb7164933317d9ac63e4d909d2f43331fe6a81
Anak7n <buzzzzx233@gmail.com>
2021-03-29 15:15:15 +0800

    feat: add divide and conquer algo version of binary search

--- src/js/algorithm/searching/binary-search.js
+++ src/js/algorithm/searching/binary-search.js
@@ -24,8 +24,33 @@ const binarySearch = (array, element) => {
   return DOES_NOT_FOUND;
 };
 
+// 递归版本，分治算法
+const binarySearchRecursive = (array, element) => {
+  const sortedArr = quickSort(array);
+  let low = 0;
+  let high = sortedArr.length - 1;
+
+  return binarySearchHelper(array, element, low, high);
+};
+
+const binarySearchHelper = (array, element, low, high) => {
+  let middle = Math.floor((low + high) / 2);
+  let middleVal = array[middle];
+  if (low <= high) {
+    if (element < middleVal) {
+      return binarySearchHelper(array, element, low, middle - 1);
+    } else if (element > middleVal) {
+      return binarySearchHelper(array, element, middle + 1, high);
+    } else {
+      return middle;
+    }
+  }
+
+  return DOES_NOT_FOUND;
+};
+
 const DOES_NOT_FOUND = -1;
 
 // test
 const array = [2, 3, 7, 4, 1, 6, 9, 5, 8];
-console.log(binarySearch(array, 100));
+console.log(binarySearchRecursive(array, 8));
