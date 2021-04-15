f5503798462bb523b308bf0d188d36dc43a72d78
Anak7n <buzzzzx233@gmail.com>
2021-04-15 22:50:10 +0800

    feat: add 36

--- src/the-sword-refers-to-offer/36-二叉搜索树与双向链表.js
+++ src/the-sword-refers-to-offer/36-二叉搜索树与双向链表.js
@@ -10,4 +10,36 @@
  * @param {Node} root
  * @return {Node}
  */
-const treeToDoublyList = function (root) {};
+const treeToDoublyList = function (root) {
+  if (root == null) {
+    return root;
+  }
+
+  let head;
+  let pre;
+
+  function dfs(root) {
+    if (root == null) {
+      return;
+    }
+
+    dfs(root.left);
+
+    if (pre == null) {
+      head = root;
+    } else {
+      pre.right = root;
+    }
+    root.left = pre;
+    pre = root;
+
+    dfs(root.right);
+  }
+
+  dfs(root);
+
+  head.left = pre;
+  pre.right = head;
+
+  return head;
+};
