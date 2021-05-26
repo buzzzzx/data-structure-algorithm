b08e2a3cfc071df1a5a5e8820c3758ad8da6725b
Anak7n <buzzzzx233@gmail.com>
2021-05-27 00:45:54 +0800

    feat: 剑指 Offer 二刷 44

--- src/the-sword-refers-to-offer-1/56-2-数组中数字出现的次数.js
+++ src/the-sword-refers-to-offer-1/56-2-数组中数字出现的次数.js
@@ -2,4 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-const singleNumber = function (nums) {};
+const singleNumber = function (nums) {
+  const tmp = [];
+  for (let num of nums) {
+    for (let i = 0; i < 32; i += 1) {
+      tmp[i] = tmp[i] == null ? num & 1 : tmp[i] + (num & 1);
+      num >>= 1;
+    }
+  }
+
+  let res = 0;
+  for (let i = 31; i >= 0; i -= 1) {
+    res <<= 1;
+    res |= tmp[i] % 3;
+  }
+
+  return res;
+};
+
+console.log(singleNumber([3, 4, 3, 3]));
