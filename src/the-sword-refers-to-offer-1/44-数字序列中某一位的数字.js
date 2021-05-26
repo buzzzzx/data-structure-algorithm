a52925f4319c7086e26306b3985c28e54f501d94
Anak7n <buzzzzx233@gmail.com>
2021-05-26 23:47:21 +0800

    feat: 剑指 Offer 二刷 44

--- src/the-sword-refers-to-offer-1/44-数字序列中某一位的数字.js
+++ src/the-sword-refers-to-offer-1/44-数字序列中某一位的数字.js
@@ -2,4 +2,18 @@
  * @param {number} n
  * @return {number}
  */
-const findNthDigit = function (n) {};
+const findNthDigit = function (n) {
+  let digit = 1;
+  let start = 1;
+  let count = 9;
+
+  while (n > count) {
+    n -= count;
+    digit += 1;
+    start *= 10;
+    count = digit * start * 9;
+  }
+
+  let num = start + Math.floor((n - 1) / digit);
+  return parseInt(num.toString()[(n - 1) % digit]);
+};
