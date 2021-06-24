c6f52b766733838ba17b78551ad8381a6955f818
Anak7n <buzzzzx233@gmail.com>
2021-06-24 23:56:43 +0800

    feat: leetcode101 动态规划 322

--- src/leetcode101/动态规划/322-零钱兑换.js
+++ src/leetcode101/动态规划/322-零钱兑换.js
@@ -4,22 +4,20 @@
  * @return {number}
  */
 const coinChange = function (coins, amount) {
-  const len = coins.length;
   const dp = [];
-  for (let i = 0; i <= len; i += 1) {
-    for (let j = 0; j <= amount; j += 1) {
-      dp[i][j] = 0;
-    }
+  for (let i = 0; i <= amount; i += 1) {
+    dp[i] = amount + 1;
   }
 
-  for (let i = 1; i <= len; i += 1) {
-    for (let j = 0; j <= amount; j += 1) {
-      dp[i][j] = dp[i - 1][j];
-      if (coins[i] <= j) {
-        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - coins[i]] + 1);
+  dp[0] = 0;
+  for (let i = 1; i <= amount; i += 1) {
+    for (let coin of coins) {
+      if (i >= coin) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
       }
     }
   }
-
-  return -1;
+  return dp[amount] === amount + 1 ? -1 : dp[amount];
 };
+
+console.log(coinChange([5, 2, 1], 11));
