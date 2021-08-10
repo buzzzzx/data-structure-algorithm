3ba3f2f3543287705a838d4d74777271ce958b1d
Anak7n <buzzzzx233@gmail.com>
2021-08-10 23:44:25 +0800

    feat: 字节 70 206 209 704

---
+++ src/interview/字节/206-反转链表.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function (head) {
+  if (head == null || head.next == null) {
+    return head;
+  }
+  let prev = head;
+  let curr = head.next;
+  let next;
+  while (curr) {
+    next = curr.next;
+    curr.next = prev;
+    if (prev === head) {
+      prev.next = null;
+    }
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+};
